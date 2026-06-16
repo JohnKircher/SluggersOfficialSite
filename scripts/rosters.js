ce053377c924353e8c2617d0793405c946481e5e
John Kircher <50682462+JohnKircher@users.noreply.github.com>
2026-06-15 22:34:52 -0400

    Season 8 Ready

--- scripts/rosters.js
+++ scripts/rosters.js
@@ -1,10 +1,113 @@
 window.teamRosters = {
-  "Tom": ["Bowser", "Birdo", "Kritter", "Daisy", "Magikoopa", "Goomba", "Monty Mole", "Pink Yoshi", "Tung Tung Tung Sahur", "Charlie Kirk", "Semenlad"],
-  "HarryKirch": ["Donkey Kong", "Funky Kong", "Wario", "Waluigi", "Yellow Magikoopa", "Diddy Kong", "Bowser Jr", "Koopa Troopa", "Dixie Kong", "Miss Hot", "Yellow Yoshi"],
-  "Julian": ["Toad", "Red Kritter", "Shy Guy", "Red Koopa Paratroopa", "Red Koopa Troopa", "Red Magikoopa", "Red Noki", "Red Yoshi", "Black Shy Guy", "Unc", "Genghis Khan"],
-  "Jmo": ["Petey Piranha", "Boomerang Bro", "Dark Bones", "Dry Bones", "Green Dry Bones", "Boo", "Blooper", "Light Blue Yoshi", "Tiny Kong", "Tobey Maguire", "Snape"],
-  "Kircher": ["Fire Bro", "Red Pianta", "Blue Dry Bones", "Blue Kritter", "Blue Yoshi", "Toadsworth", "Blue Shy Guy", "Noki", "Mr. Incredible", "Frozone", "Syndrome"],
-  "Carby": ["Purple Toad", "Hammer Bro", "Toadette", "Pianta", "Green Toad", "Green Magikoopa", "Green Shy Guy", "Green Noki", "Koopa Paratroopa", "KevinG", "Doc Brown"],
-  "BenT": ["Wiggler", "Yellow Pianta", "Yellow Toad", "Blue Toad", "Baby DK", "Paragoomba", "Yellow Shy Guy", "Baby Peach", "Brown Kritter", "Lebron James"],
-  "BenR": ["Mario", "King K Rool", "King Boo", "Peach", "Luigi", "Yoshi", "Baby Mario", "Baby Luigi", "Baby Daisy", "Rizzler", "Matt"]
+  "BenR": [
+    "Bowser",
+    "Green Dry Bones",
+    "Blue Dry Bones",
+    "Magikoopa",
+    "Blue Kritter",
+    "Waluigi",
+    "Blue Shy Guy",
+    "Paragoomba",
+    "Rizzler",
+    "Pops",
+    "Matt"
+  ],
+
+  "BenT": [
+    "Donkey Kong",
+    "Hammer Bro",
+    "Funky Kong",
+    "Purple Toad",
+    "Baby DK",
+    "Wario",
+    "Yellow Magikoopa",
+    "Black Shy Guy",
+    "Dixie Kong",
+    "Lebron James",
+    "Borat"
+  ],
+
+  "Carby": [
+    "Petey Piranha",
+    "Kritter",
+    "Wiggler",
+    "Yellow Toad",
+    "Toadette",
+    "Blooper",
+    "Yellow Yoshi",
+    "Baby Peach",
+    "Livvy Dunne",
+    "Carby",
+    "Lara Croft"
+  ],
+
+  "Tom": [
+    "Pianta",
+    "Boomerang Bro",
+    "Dry Bones",
+    "Daisy",
+    "Blue Yoshi",
+    "Light Blue Yoshi",
+    "Baby Daisy",
+    "Baby Luigi",
+    "Tung Tung Tung Sahur",
+    "Charlie Kirk",
+    "KevinG"
+  ],
+
+  "HarryKirch": [
+    "Birdo",
+    "Fire Bro",
+    "Luigi",
+    "Yoshi",
+    "Green Magikoopa",
+    "Bowser Jr",
+    "Green Shy Guy",
+    "Yellow Shy Guy",
+    "Miss Hot",
+    "Dwayne Wade",
+    "Winnie The Pooh"
+  ],
+
+  "Jmo": [
+    "Yellow Pianta",
+    "King K Rool",
+    "Brown Kritter",
+    "King Boo",
+    "Blue Toad",
+    "Toadsworth",
+    "Peach",
+    "Boo",
+    "Red Noki",
+    "Badlands",
+    "Diminutive"
+  ],
+
+  "Kircher": [
+    "Red Pianta",
+    "Dark Bones",
+    "Mario",
+    "Green Toad",
+    "Red Yoshi",
+    "Red Magikoopa",
+    "Diddy Kong",
+    "Tiny Kong",
+    "Baby Mario",
+    "Pink Yoshi",
+    "MJ HeeHee"
+  ],
+
+  "Julian": [
+    "Toad",
+    "Red Kritter",
+    "Shy Guy",
+    "Red Koopa Paratroopa",
+    "Red Koopa Troopa",
+    "Koopa Paratroopa",
+    "Koopa Troopa",
+    "Goomba",
+    "Monty Mole",
+    "Unc",
+    "Ghandi"
+  ]
 };
